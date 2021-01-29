e6101e7fb7ae39697e9d248638b40d587796f957
linda <linda@DESKTOP-TKNBK7A>
2021-01-28 19:10:18 -0600

    working on multithreading

---
+++ InboundDataController/src/main/java/eyelind/runner/NewInboundFileThread.java
@@ -0,0 +1,13 @@
+package eyelind.runner;
+
+public class NewInboundFileThread extends Thread {
+
+	public void run() {
+		System.out.println("New thread started...");
+	}
+	
+	 public boolean getShutdownHook() {
+         return this.getShutdownHook();
+     }
+	 
+}
--- InboundDataController/src/main/java/eyelind/runner/ServiceRunner.java
+++ InboundDataController/src/main/java/eyelind/runner/ServiceRunner.java
@@ -2,9 +2,38 @@ package eyelind.runner;
 
 public class ServiceRunner {
 
+	
+	private static boolean running;
+	
 	public static void main(String[] args) {
-		//init
 
+		Runtime.getRuntime().addShutdownHook(new NewInboundFileThread());
+		boolean running = true;
+		
+		while (running) {
+			
+			try {
+				
+				setShutdownHook(true);
+			} catch (Exception e) {
+				setShutdownHook(false);
+			}
+			
+			running = getShutdownHook();
+			if (!running) {
+				System.out.println("Shutdown hook true, terminating program safely...");
+				System.exit(1);
+			}
+		}
+		
+	}
+
+	private static boolean getShutdownHook() {
+		return running;
+	}
+	
+	private static void setShutdownHook(boolean isRunning) {
+		running = isRunning;
 	}
 
 }
